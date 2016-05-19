d8e554b63f1f022c28e0f9ea605e95b3637c6ab6
Sanita <szayshlyy@gmail.com>
2016-05-18 21:28:59 -0700

    Sanita_Module_6

---
+++ .eslintrc.js
@@ -0,0 +1,39 @@
+module.exports = {
+    "env": {
+        "node": true,
+        "commonjs": true
+    },
+    "extends": "eslint:recommended",
+    "rules": {
+        "indent": [
+            "error",
+            2
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ],
+        "semi-spacing": "error",
+         "comma-dangle": [
+            "error",
+            "never"
+        ],
+        "no-trailing-spaces": "error",
+        "object-curly-spacing": [
+            "error",
+            "always"
+        ],
+        "newline-before-return": [
+            "error"
+        ]
+
+    }
+};
--- routes/truckRoutes.js
+++ routes/truckRoutes.js
@@ -1,2 +1,104 @@
-// Connect to mongodb in this module as this is where you'll be making creat/read/delete calls to your database
-// use 'mongodb://localhost/foodTruckAPI' for your mongoose connection string
+var express = require('express');
+var bodyParser = require('body-parser');
+var urlEncoded = bodyParser.urlencoded({ extended: false });
+
+var Truck = require('../models/truckModel');
+
+var router = express.Router();
+
+router.route('/')
+  .get(function (request, response) {
+    Truck.find(function(error, results){
+      if(error){
+        response.status(500).send(error);
+      }else{
+        response.send(results);
+      }
+    })
+
+  })
+
+  .post(urlEncoded, function (request, response) {
+    var newTruck = new Truck (request.body);
+
+    newTruck.save(function(error, newTruck){
+      if(error){
+        response.status(500).send(error);
+      }else {
+        response.status(201).send(newTruck);
+      }
+    })
+  });
+
+router.route('/:truckId')
+  .get(function (request, response) {
+    var truckId = request.params.truckId;
+    Truck.findById(truckId, function (error, result){
+      if (error) {
+        response.status(500).send(error);
+      } else {
+        response.send(result);
+      }
+
+    });
+  })
+
+  .put(urlEncoded, function (request, response) {
+    var truckId = request.params.truckId;
+    var update = {
+      name: request.body.name,
+      foodType: request.body.foodType,
+      schedule: request.body.schedule,
+      payment: request.body.payment,
+      description: request.body.description,
+      website: request.body.website,
+      Facebook: request.body.Facebook,
+      Twitter: request.body.Twitter
+    };
+
+    Truck.findByIdAndUpdate(truckId, update, function (error, affected, result) {
+      if (error) {
+        response.status(500).send(error);
+      } else {
+        response.send(result);
+      }
+    });
+  })
+
+  .delete(function (request, response) {
+    var truckId = request.params.truckId;
+
+    Truck.findByIdAndRemove(truckId, function (error) {
+      if (error) {
+        response.status(500).send(error);
+      } else {
+        response.status(204).send('removed');
+      }
+    });
+  });
+
+// router.route('/food-types')
+//   .get(function (request, response){
+//     Truck.distinct('foodType', function (error, results) {
+//       if (error) {
+//         response.status(500).send(error);
+//       } else {
+//         response.send(results);
+//       }
+
+//     })
+//   });
+
+// router.route('/food-types/:type')
+//   .get(function (request, response) {
+//     var foodType = request.params.type;
+//     Truck.find({ foodType: { $in: [foodType] } }, function (error, results) {
+//       if (error) {
+//         response.status(500).send(error);
+//       } else {
+//         response.send(results);
+//       }
+//     });
+//   });
+
+module.exports = router;
